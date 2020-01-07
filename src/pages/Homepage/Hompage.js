00d29add7ccfd761006d08ad95603a22fa130485
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-07 22:51:29 +0000

    working on the project

--- src/pages/Homepage/Hompage.js
+++ src/pages/Homepage/Hompage.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "./homepage.styles.scss"
-import MenuItem from "../../components/menu-item/menu-item" 
 import Directory from "../../components/directory/Directory"
 
 const Homepage = () =>(
