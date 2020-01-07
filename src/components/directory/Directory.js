640e48b9db2e893b28bbc0c689c09f0cea613af9
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-07 22:55:32 +0000

    working on the project

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -43,7 +43,7 @@ class Directory extends Component {
     render() { 
         return ( 
                 <div className="directory-menu">
-                    {this.state.sections.map(({title,imageUrl,id})=><MenuItem key={id} title={title} />)}
+                    {this.state.sections.map(({title,imageUrl,id})=><MenuItem key={id} title={title} imageUrl={imageUrl}/>)}
                 
                 </div>
 
