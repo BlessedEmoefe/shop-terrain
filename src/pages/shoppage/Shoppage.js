d3939bbe9b27d83b54fb7355d360a0649ef063a8
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-08 22:33:12 +0000

    working on the shop page

--- src/pages/shoppage/Shoppage.js
+++ src/pages/shoppage/Shoppage.js
@@ -1,1 +1,14 @@
-import React from "react"
+import React,{ Component} from "react"
+
+class ShopPage extends Component{
+    state={
+
+    };
+
+    render{
+        return(
+
+        )
+    }
+};
+export default ShopPage;
