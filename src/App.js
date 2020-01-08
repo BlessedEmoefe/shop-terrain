40dcf5ded447d484fa0fdf416d3ea68deab4f670
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-08 16:12:03 +0000

    started working on routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage/Hompage';
+import { Route } from "react-router-dom"
 
 function App() {
+  
   return (
     <div>
       <Homepage />
