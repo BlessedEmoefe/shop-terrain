2768a2be0bc02ef44979cf3b581d41a70f3a7f80
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-08 20:12:57 +0000

    started working on routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage/Hompage';
-import { Route } from "react-router-dom"
+import { Route,Link } from "react-router-dom"
+
+const Hatspage=()=>{
+return(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+};
 
 function App() {
-  
   return (
     <div>
       <Homepage />
--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -44,7 +44,7 @@ class Directory extends Component {
         return ( 
                 <div 
                 className="directory-menu">
-                    {this.state.sections.map(({title,imageUrl,id,size})=><MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)}
+                    {this.state.sections.map(({id,...othersectionitems})=><MenuItem key={id} {...othersectionitems}/>)}
                 
                 </div>
 
