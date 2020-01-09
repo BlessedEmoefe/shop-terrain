f276d24bfc57a68a4165cbfdc0f1b21f33d20563
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-01-09 20:55:49 +0000

    working on the preview collection and shop page

---
+++ src/components/collection-preview/collection-preview.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./collection-preview.styles.scss"
+
+const CollectionPreview = () =>(
+<div className="collection-preview">
+<h1>Title</h1>
+<div className="preview">
+
+</div>
+</div>
+)
+export default CollectionPreview;
