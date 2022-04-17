2eae04de5598b2bad293f303fc2a0984a586446c
blemo <blessedemoefe1122334455@gmail.com>
2022-04-17 23:49:37 +0100

    adding changes

--- src/pages/HatsPage/Hats.js
+++ src/pages/HatsPage/Hats.js
@@ -1,9 +1,10 @@
 import React from "react"
 
-const Hats = () =>(
-    <div>
-        hello this is the hatspage
-        <p>Hello this is the hats page</p>
-    </div>
-)
+const Hats = () => (
+  <div>
+    hello this is the hatspage
+    <p>Hello this is the hats page</p>
+    <p>Hello this is the hats page</p>
+  </div>
+);
 export default Hats;
